5ead645cda87928fd647944ee5fad9fa62748c8f
andmansim <91722324+andmansim@users.noreply.github.com>
2022-04-16 19:59:05 +0200

    map

--- Py/panda.py
+++ Py/panda.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import matplotlib as plt
+import numpy as np
+
 #leer con pandas
 df = pd.read_csv('EILU_MAST_2019.csv', delimiter="\t", encoding='UTF-8')
 print('---------------LEER---------------------')
@@ -32,7 +35,6 @@ df['EDAD'].mean() #devuelve la media de los datos (numericos) en la columna
 df['EDAD'].std() #devuelve la desviacion tipica de los datos numericos
 df['EDAD'].describe() #devuelve una serie con un resumen de algunos de los datos anteriores, como la media, la desviacion tipica...
 
-
 #asiganción
 print('--------------ASIGNACIÓN---------------')
 df1 = df.copy()#copia del DataFrame, para no modificarlo
@@ -41,7 +43,16 @@ print(df1.head(1))
 df1.loc[0, 'EDAD'] = 3
 print(df1.head(1))
 
+#matplotlib
+fig = plt.figure() #creamos la  figura, es donde se construyen todos los gráficos 
+ax = fig.add_subplot(1, 1, 1) # marco
+ax.set_title('Título del gráfico')
+ax.set_xlabel('Etiqueta para el eje X')
+ax.set_ylabel('Etiqueta para el eje Y')
+ax.xaxis.grid(True) #Poner rejillas eje x
+ax.yaxis.grid(True) #Poner rejillas eje y
 
-#agrupacion 
-print("----------------------------AGRUPACION-----------------------------")
-df.groupby()
+
+
+
+
