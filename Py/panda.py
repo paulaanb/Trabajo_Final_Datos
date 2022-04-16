b6d1755d5aceb92f268908cdc0afca6b5bfaa7d1
andmansim <91722324+andmansim@users.noreply.github.com>
2022-04-16 19:14:11 +0200

    separación

--- Py/panda.py
+++ Py/panda.py
@@ -1,6 +1,7 @@
 import pandas as pd
 #leer con pandas
 df = pd.read_csv('EILU_MAST_2019.csv', delimiter="\t", encoding='UTF-8')
+print('---------------LEER---------------------')
 print(df)
 
 #escribir con pandas
@@ -9,9 +10,12 @@ prueba.to_csv('prueba.csv')
 
 #Indexación
 #filas
+print('--------------FILAS----------------')
 print (df.iloc[0])
 #columnas
+print('----------------COLUMNA----------------------')
 print(df['EDAD'])
 
 #seleccion
+print('------------SELECCIÓN--------------------')
 print(df['EDAD']== 1)
