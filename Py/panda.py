b5871dfcc9736e504651a952ba5e6121aa088e6e
andmansim <91722324+andmansim@users.noreply.github.com>
2022-04-16 19:28:12 +0200

    asignación

--- Py/panda.py
+++ Py/panda.py
@@ -19,3 +19,12 @@ print(df['EDAD'])
 #seleccion
 print('------------SELECCIÓN--------------------')
 print(df['EDAD']== 1)
+
+#asiganción
+#copia del DataFrame, para no modificarlo
+print('--------------ASIGNACIÓN---------------')
+df1 = df.copy()
+print(df1.equals(df)) #comprobamos que son iguales
+print(df1.head(1))
+df1.loc[0, 'EDAD'] = 3
+print(df1.head(1))
