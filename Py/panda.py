61ccdea0acb616ca4e273f8dd57307a477c50d4c
andmansim <91722324+andmansim@users.noreply.github.com>
2022-04-16 18:55:04 +0200

    a

--- Py/panda.py
+++ Py/panda.py
@@ -1,8 +1,5 @@
 import pandas as pd
 #leer con pandas
 df = pd.read_csv('EILU_MAST_2019.csv', delimiter="\t", encoding='UTF-8')
-print(df)
+#print(df)
 
-#escribir con pandas
-prueba = pd.DataFrame(['Hola'], columns=['Prueba'])
-prueba.to_csv('prueba.csv')
