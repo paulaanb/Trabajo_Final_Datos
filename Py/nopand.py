36d08c25d13d0db1e3a0499afaad48c9c2627ad1
andmansim <91722324+andmansim@users.noreply.github.com>
2022-05-12 14:01:14 +0200

    mejora al leer el fichero

--- Py/nopand.py
+++ Py/nopand.py
@@ -8,6 +8,11 @@ Underfitting y Overfitting(Ajuste su modelo para un mejor rendimiento)
 Bosques aleatorios(Usando un algoritmo de aprendizaje automático más sofisticado)
 '''
 #abrir el fichero csv
+fichero = []
 with open('EILU_MAST_2019.csv', encoding='UTF-8') as f:
-    leer = csv.reader(f, delimiter = '\t')
-print(leer)
+    leer = csv.DictReader(f, delimiter = '\t')
+    for i in leer:
+        fichero.append(i)
+for x in range(len(fichero)):
+    print(fichero[x])
+    print('\n')
