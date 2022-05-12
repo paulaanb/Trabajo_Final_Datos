882741094de810ebecc11440332e80c3dfd2eb59
andmansim <91722324+andmansim@users.noreply.github.com>
2022-05-12 13:55:24 +0200

    enunciado

---
+++ Py/nopand.py
@@ -0,0 +1,8 @@
+'''
+Cómo funcionan los modelos(El primer paso si eres nuevo en el aprendizaje automático)
+Exploración básica de datos(Cargue y comprenda sus datos)
+Su primer modelo de aprendizaje automático (Construyendo tu primer modelo)
+Modelo de validación(Mida el rendimiento de su modelo, para que pueda probar y comparar alternativas)
+Underfitting y Overfitting(Ajuste su modelo para un mejor rendimiento)
+Bosques aleatorios(Usando un algoritmo de aprendizaje automático más sofisticado)
+'''
