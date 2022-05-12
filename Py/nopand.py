a7d49e28065739b7dd1f6f7066ce3de678735a4b
andmansim <91722324+andmansim@users.noreply.github.com>
2022-05-12 13:58:52 +0200

    abrir y leer los datos

--- Py/nopand.py
+++ Py/nopand.py
@@ -1,3 +1,4 @@
+import csv
 '''
 Cómo funcionan los modelos(El primer paso si eres nuevo en el aprendizaje automático)
 Exploración básica de datos(Cargue y comprenda sus datos)
@@ -6,3 +7,7 @@ Modelo de validación(Mida el rendimiento de su modelo, para que pueda probar y 
 Underfitting y Overfitting(Ajuste su modelo para un mejor rendimiento)
 Bosques aleatorios(Usando un algoritmo de aprendizaje automático más sofisticado)
 '''
+#abrir el fichero csv
+with open('EILU_MAST_2019.csv', encoding='UTF-8') as f:
+    leer = csv.reader(f, delimiter = '\t')
+print(leer)
